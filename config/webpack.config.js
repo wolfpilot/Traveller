e5a6fb460146b25752c31839e0fbdbc31acfef49
Razvan Negrea <rznart@gmail.com>
2017-04-24 02:23:20 +0100

    Set up CSS and SASS loaders with Hot Reload

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,23 +5,23 @@ export default {
 	entry: [
 		'webpack-hot-middleware/client?reload=true',
 		path.join(__dirname, '../src/index.js'),
+		path.join(__dirname, '../src/styles/style.scss')
 	],
 	output: {
 		filename: 'bundle.js',
 		path: '/',
 		publicPath: '/'
 	},
+	module: {
+		loaders: [
+			{ test: /\.jsx?$/, exclude: /node_modules/,	loader: 'babel-loader' },
+			{ test: /\.(sass|scss)$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
+		]
+	},
 	plugins: [
 		new webpack.NoEmitOnErrorsPlugin(),
 		new webpack.HotModuleReplacementPlugin()
 	],
-	module: {
-		loaders: [{
-			test: /\.jsx?$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader'
-		}]
-	},
 	resolve: {
 		extensions: [
 			' ',
