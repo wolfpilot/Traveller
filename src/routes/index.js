a23999367cf7841be5152f6330c8e724bfb39bdd
Razvan Negrea <rznart@gmail.com>
2017-05-01 01:16:00 +0100

    Add History

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import history from './history';
 
 import HomePage from './HomePage';
 import PlacesPage from './PlacesPage';
@@ -10,10 +11,16 @@ import NotFoundPage from './NotFoundPage';
 
 export default (
 
-	<Switch>
-		<Route exact={true} path="/" component={HomePage} />
-		<Route path="/places" component={PlacesPage} />
-		<Route component={NotFoundPage} />
-	</Switch>
+	<Router history={history}>
+		<div>
+			<main className="main">
+				<Switch>
+					<Route exact={true} path="/" component={HomePage} />
+					<Route path="/places" component={PlacesPage} />
+					<Route component={NotFoundPage} />
+				</Switch>
+			</main>
+		</div>
+	</Router>
 
 );
